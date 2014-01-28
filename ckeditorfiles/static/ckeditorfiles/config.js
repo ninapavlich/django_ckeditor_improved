cfeccd3b6f885f21dbd662eca0b3430a24f9dd4f
Sara Perry <sara@cgpartnersllc.com>
2014-01-28 10:53:15 -0500

    adding lineutils to config file

--- ckeditorfiles/static/ckeditorfiles/config.js
+++ ckeditorfiles/static/ckeditorfiles/config.js
@@ -26,6 +26,7 @@ CKEDITOR.editorConfig = function( config ) {
 	];
 	config.extraPlugins = 'image2';
 	config.extraPlugins = 'widget';
+	config.extraPlugins = 'lineutils';
 
 	// The default plugins included in the basic setup define some buttons that
 	// we don't want too have in a basic editor. We remove them here.
