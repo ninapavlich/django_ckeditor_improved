7397b4db1ba44eb528b06f572049fe83923d60f4
Nina Pavlich <nina@ninalp.com>
2015-04-16 18:11:10 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 #this is a test
 setup(name = 'django_ckeditor_improved',
       description = 'CKEditor bundled as a django staticfiles app.',
-      version = '1.2',
+      version = '1.2.1',
       url = 'https://github.com/ninapavlich/django_ckeditor_improved',
       author = 'Nina Pavlich',
       author_email='nina@ninalp.com',
